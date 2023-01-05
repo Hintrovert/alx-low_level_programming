8fd2954269e9857c96e805e33ccf6dfd2be60204
Hintrovert <chymexky@gmail.com>
2023-01-05 00:06:10 -0800

    1-print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-/**
- * * _print_rev_recursion - prints a string in reverse
- * * @s: a string
- * * Return: void
- * */
+/*
+ * _print_rev_recursion - prints a string in reverse
+ * @s: a string
+ * Return: void
+ */
 
 void _print_rev_recursion(char *s)
 {
