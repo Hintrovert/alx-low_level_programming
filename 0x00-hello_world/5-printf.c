286bc641719f133ce55c1eea3ecc37b71d7e2903
Hintrovert <chymexky@gmail.com>
2023-02-16 20:25:34 -0800

    5-printf.c commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#!/bin/bash
+#include <stdio.h>
 /**
  * main - write a program that prints "with proper grammar, but the outcome is a piece of art,"
  * return 0 if the program works and others if any error
