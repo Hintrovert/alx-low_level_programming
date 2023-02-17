802efccd720ce04477cac1cef508577f1db9fc3e
Hintrovert <chymexky@gmail.com>
2023-02-16 20:22:56 -0800

    adding two new commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main - prints the sentence prints the phrase "Programming is like building a multilingual puzzle
+ *  Return: 0 if exited properly, others in any error.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/**
+ * main - write a program that prints "with proper grammar, but the outcome is a piece of art,"
+ * return 0 if the program works and others if any error
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}	
