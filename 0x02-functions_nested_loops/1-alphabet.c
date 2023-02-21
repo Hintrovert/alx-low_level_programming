d8dd4990516b2c211df5d6e2f6ee1071c505e95d
Hintrovert <chymexky@gmail.com>
2023-02-21 15:04:17 -0800

    1-alphabet.c recommit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,9 @@
 /**
  * main - Write a function that prints 10 times the alphabet in lowercase.
  *
- * Return: Always 0
+ *
  */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 	char letter;
 
