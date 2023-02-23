bc98e208dbf7b9e4171865fbcce945688ea9622f
Hintrovert <chymexky@gmail.com>
2023-02-22 18:09:34 -0800

    6-abs.c commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - A function that computes the absolute value of an integer.
+ * @n: The int to be computed
+ *
+ * Return: the abs of the int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
+
